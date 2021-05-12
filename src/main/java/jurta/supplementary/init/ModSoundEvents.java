8e2bf267c261b5f5b06653b5d2529feb52162d31
Jurta <29545237+Jurtaa@users.noreply.github.com>
2021-05-12 19:28:21 +0100

    Custom sounds for Lush Stone

--- src/main/java/jurta/supplementary/init/ModSoundEvents.java
+++ src/main/java/jurta/supplementary/init/ModSoundEvents.java
@@ -6,8 +6,17 @@ import net.minecraft.util.SoundEvent;
 import net.minecraftforge.fml.RegistryObject;
 
 public class ModSoundEvents {
-    /* public static final RegistryObject<SoundEvent> TEST = Registration.SOUNDS.register("test", () ->
-            new SoundEvent(modLoc("test"))); */
+    // Lush Stone
+    public static final RegistryObject<SoundEvent> BLOCK_LUSH_STONE_BREAK = Registration.SOUNDS.register("block_lush_stone_break", () ->
+            new SoundEvent(modLoc("block.lush_stone.break")));
+    public static final RegistryObject<SoundEvent> BLOCK_LUSH_STONE_FALL = Registration.SOUNDS.register("block_lush_stone_fall", () ->
+            new SoundEvent(modLoc("block.lush_stone.fall")));
+    public static final RegistryObject<SoundEvent> BLOCK_LUSH_STONE_HIT = Registration.SOUNDS.register("block_lush_stone_hit", () ->
+            new SoundEvent(modLoc("block.lush_stone.hit")));
+    public static final RegistryObject<SoundEvent> BLOCK_LUSH_STONE_PLACE = Registration.SOUNDS.register("block_lush_stone_place", () ->
+            new SoundEvent(modLoc("block.lush_stone.place")));
+    public static final RegistryObject<SoundEvent> BLOCK_LUSH_STONE_STEP = Registration.SOUNDS.register("block_lush_stone_step", () ->
+            new SoundEvent(modLoc("block.lush_stone.step")));
 
     static void register() {}
 
--- src/main/java/jurta/supplementary/init/ModSoundType.java
+++ src/main/java/jurta/supplementary/init/ModSoundType.java
@@ -1,8 +1,12 @@
 package jurta.supplementary.init;
 
-import net.minecraft.util.SoundEvents;
 import net.minecraftforge.common.util.ForgeSoundType;
 
 public class ModSoundType {
-    public static final ForgeSoundType LUSH_STONE = new ForgeSoundType(1.0F, 1.0F, () -> SoundEvents.STONE_BREAK, () -> SoundEvents.GRASS_STEP, () -> SoundEvents.STONE_PLACE, () -> SoundEvents.STONE_HIT, () -> SoundEvents.GRASS_FALL);
+    public static final ForgeSoundType LUSH_STONE = new ForgeSoundType(1.0F, 1.0F,
+            ModSoundEvents.BLOCK_LUSH_STONE_BREAK,
+            ModSoundEvents.BLOCK_LUSH_STONE_STEP,
+            ModSoundEvents.BLOCK_LUSH_STONE_PLACE,
+            ModSoundEvents.BLOCK_LUSH_STONE_HIT,
+            ModSoundEvents.BLOCK_LUSH_STONE_FALL);
 }
